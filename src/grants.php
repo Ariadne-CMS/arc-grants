ba79cb1cc1d1eb7f5aa33b9e77620f0394fb91c4
Auke van Slooten <auke@poefke.nl>
2014-06-25 18:56:40 +0200

    changed to PSR-2 coding style

--- src/grants.php
+++ src/grants.php
@@ -17,32 +17,37 @@ namespace arc;
  *	@requires \arc\context;
  */
 
-class grants {
+class grants
+{
+    public static function getGrantsTree()
+    {
+        $context = \arc\context::$context;
+        if (!$context->arcUser) {
+            $context->arcUser = 'public';
+        }
+        if (!$context->arcGroups) {
+            $context->arcGroups  = [ 'public' ];
+        }
+        if (!$context->arcGrants) {
+            $context->arcGrants = new grants\GrantsTree( \arc\tree::expand()->cd( $context->arcPath ), $context->arcUser, $context->arcGroups );
+        }
 
-	public static function getGrantsTree() {
-		$context = \arc\context::$context;
-		if ( !$context->arcUser ) {
-			$context->arcUser = 'public';
-		}
-		if ( !$context->arcGroups ) {
-			$context->arcGroups  = [ 'public' ];
-		}
-		if ( !$context->arcGrants ) {
-			$context->arcGrants = new grants\GrantsTree( \arc\tree::expand()->cd( $context->arcPath ), $context->arcUser, $context->arcGroups );
-		}
-		return $context->arcGrants;
-	}
+        return $context->arcGrants;
+    }
 
-	public static function check( $grant ) {
-		return self::getGrantsTree()->check( $grant );
-	}
+    public static function check($grant)
+    {
+        return self::getGrantsTree()->check( $grant );
+    }
 
-	public static function cd( $path ) {
-		return self::getGrantsTree()->cd( $path );
-	}
+    public static function cd($path)
+    {
+        return self::getGrantsTree()->cd( $path );
+    }
 
-	public static function ls() {
-		return self::getGrantsTree()->ls();
-	}
+    public static function ls()
+    {
+        return self::getGrantsTree()->ls();
+    }
 
 }
